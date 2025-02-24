9dc8da22b01d2d4b81d679e5b799a71c9c750b0a
hyerin-kang <hkang0420@gmail.com>
2025-02-25 02:10:12 +0900

    2. Tanstack QueryProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "./components/layout/RootLayout";
 import HomePage from "./pages/HomePage";
 import TodoDetailPage from "./pages/TodoDetailPage";
-import TodoProvider from "./components/provider/TodoProvider";
+import QueryProvider from "./components/provider/QueryProvider";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <TodoProvider>
+      <QueryProvider>
         <Routes>
           <Route path="/" element={<RootLayout />}>
             <Route index element={<HomePage />} />
             <Route path="todos/:id" element={<TodoDetailPage />} />
           </Route>
         </Routes>
-      </TodoProvider>
+      </QueryProvider>
     </BrowserRouter>
   );
 };
