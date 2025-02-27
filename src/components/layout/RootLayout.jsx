2af11ef7c63dc0093d74f029111587774d6b3ede
hyerin-kang <hkang0420@gmail.com>
2025-02-27 23:48:39 +0900

    다크모드활성화,주스탄드 스토어

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router";
-
+// import ThemeSwitchButton from "../TemeSwitchButton";
 // Compositional Layout
 const RootLayout = () => {
   return (
-    <main className="bg-[#f6f5f8] min-h-screen">
+    <main className="bg-[#f6f5f8] min-h-screen dark:bg-slate-800">
       <div className="max-w-[768px] mx-auto p-4">
         <Outlet />
       </div>
